ffe592143c88a8e7a1eb27a7ddaa18aa50e233f6
Chriffy-Herculano <chriffy123@gmail.com>
2024-05-20 22:32:54 -0300

    Projeto finalizado, foi implementado o localStorage para guardar as tarefas

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
-var todos = [
-    {
-        text: 'Aprender HTML, CSS e Javascript',
-        done: false,
-    },
-    {
-        text: 'Aprender o básico de Vue JS',
-        done: true,
-    }
-];
+// var todos = [
+//     {
+//         text: 'Aprender HTML, CSS e Javascript',
+//         done: false,
+//     },
+//     {
+//         text: 'Aprender o básico de Vue JS',
+//         done: true,
+//     }
+// ];
 
 const todosApp = {
+
     data() {
         return {
-            todos: window.todos,
+            todos: [],
             newTodo: {
                 done: false
             }
@@ -27,14 +28,29 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 };
-                
+
+                //add no localStorage do browser
+                localStorage.setItem("Tarefas", JSON.stringify(this.todos));
             } else {
                 alert('Preencha o campo!');
             }
-            
+        }, // So sera chamado quando for requisitado
+        storeTodos () {
+            localStorage.setItem("Tarefas", JSON.stringify(this.todos));
+            console.log('Foi');
         }
-    }
+    }, //Depois de criar a aplicação e antes de abastecer o HTML
+    created() {
+        this.todos = localStorage.getItem("Tarefas") ? JSON.parse(localStorage.getItem("Tarefas")) : this.todos;
+    },
+    updated() {
+        // Pode ser usado em uma aplicação simples, a cada interação na pagina ele vai ser chamado 
+        //localStorage.setItem("Tarefas", JSON.stringify(this.todos));
+    },
 };
 
+// Inicia a aplicação injetando os dados no HTML
 Vue.createApp(todosApp).mount('#app');
+
+
 
